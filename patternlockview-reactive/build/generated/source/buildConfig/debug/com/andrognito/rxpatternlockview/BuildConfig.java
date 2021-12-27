617c116e46e8c7da391a40bf7048e0edfabcfbaf
evan <chanjungskim@gmail.com>
2021-12-27 18:18:40 +0900

    Fix: runnable

---
+++ patternlockview-reactive/build/generated/source/buildConfig/debug/com/andrognito/rxpatternlockview/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.andrognito.rxpatternlockview;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.andrognito.rxpatternlockview";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ patternlockview/build/generated/source/buildConfig/debug/com/andrognito/patternlockview/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.andrognito.patternlockview;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.andrognito.patternlockview";
+  public static final String BUILD_TYPE = "debug";
+}
